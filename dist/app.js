522a52b594e278b794355b2cadcf749fdc1383cf
iylinkim <kiylin94@naver.com>
2021-03-12 21:26:47 +0900

    create PageComponent

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
-"use strict";
+import { PageComponent } from "./components/page.js";
+export class App {
+    constructor(appRoot) {
+        this.page = new PageComponent();
+        this.page.attachTo(appRoot);
+    }
+}
+new App(document.querySelector(".document"));
